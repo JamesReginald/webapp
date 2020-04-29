28109fda284fb7d677741c4c83f42200310a7148
JamesReginald <jim@bpool.nl>
2020-04-29 23:40:18 +0200

    add

---
+++ src/main/java/com/webShopApp/service/RideService.java
@@ -0,0 +1,21 @@
+package com.webShopApp.service;
+
+import java.util.List;
+
+import com.webShopApp.model.Ride;
+
+public interface RideService {
+
+	Ride createRide(Ride ride);
+
+	List<Ride> getRides();
+
+	Ride getRide(Integer id);
+
+	Ride updateRide(Ride ride);
+
+	void batch();
+
+	void deleteRide(Integer id);
+
+}
